f7fecfb55bbda28c48a149a4e64e7a0e83ec0db0
yarubalshabibi-hub <yarub.alshabibi@gmail.com>
2026-06-07 08:26:26 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mini_Flight_Management_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
