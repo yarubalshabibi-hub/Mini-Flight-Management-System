73c83808e50decaa532b593d1829a701563d3f8a
yarubalshabibi-hub <yarub.alshabibi@gmail.com>
2026-06-07 08:55:01 +0400

    I finished write the variables.

--- Program.cs
+++ Program.cs
@@ -2,9 +2,20 @@
 {
     internal class Program
     {
+        static List<string> passengerNames = new List<string>();
+        static List<string> ticketNumbers = new List<string>();
+        static string[] flightNumbers = { "OA101", "OA102", "OA103", "OA104", "OA105", "OA106" };
+        static List<string> availableDates = new List<string>();
+        static Dictionary<string, string> bookingRecord = new Dictionary<string, string>();
+        static Queue<string> checkedInQueue = new Queue<string>();
+        static Stack<string> boardingStack = new Stack<string>();
+        static List<string> cancelledTickets = new List<string>();
+        static Dictionary<string, string> passengerSeatMap = new Dictionary<string, string>();
+        static Queue<string> waitlistQueue = new Queue<string>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
